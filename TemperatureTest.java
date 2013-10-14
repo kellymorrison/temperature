09ba8ee4c4b7fb289475a800d8c05dbec61420f9
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-10-14 09:05:42 -0400

    Tested getValue() method

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -9,16 +9,20 @@ import static org.junit.Assert.* ;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+	public double PRECISION = 0.000001;
+	
+	@Test
+	public void testGetValue() {
+		Temperature boundaryTest = new Temperature(0.0,Temperature.Units.KELVIN);
+		assertEquals("Temperature should equals 0.0 Kelvin", 0.0, boundaryTest.getValue(), PRECISION);
+		
+		Temperature negativeTest = new Temperature(-45.0, Temperature.Units.CELSIUS);
+		assertEquals("Temperature should equal -45.0 Celsius", -45.0, negativeTest.getValue(), PRECISION);
+		
+		Temperature intTest = new Temperature(30, Temperature.Units.FAHRENHEIT);
+		assertEquals("Temperature should equal 30.0 Celcius", 30.0, intTest.getValue(), PRECISION);
+		
+		Temperature precisionTest = new Temperature(60.1234567, Temperature.Units.CELSIUS);
+		assertEquals("Temperature should equal 60.123456", 60.123456, precisionTest.getValue(), PRECISION);
+	}
 }
